17f68cf949ccf8d55dc32b420daa03e3a476caa7
Konstantin Sozinov <sozinov@kth.se>
2017-04-20 15:20:20 +0200

    dd2424 lab2 momentum update and finding hyper-parameters

--- DD2424 - Deep Learning in Data Science /lab2/lab2.py
+++ DD2424 - Deep Learning in Data Science /lab2/lab2.py
@@ -82,22 +82,37 @@ class DataLoader:
 class NeuralNetwork:
 
 	NODES_IN_HIDDEN = 50
-	LAMBDA = 0
-	ETA = 0.01
-	N_EPOCHS = 20
+	N_EPOCHS = 5
+	
+	# Hyper-parameters
+	MU = 0.9
+	DECAY_RATE = 0.95
 
-	def __init__(self,data_dim, num_labels):
+	def __init__(self,data_dim, num_labels, eta=0.1, lmbd=0.000001):
 		# Note from stanford course:
 		# It turns out that we can normalize the variance of each neuron's output to 
 		# 1 by scaling its weight vector by the square root of its fan-in (i.e. its number of inputs). 
 		# That is, the recommended heuristic is to initialize each neuron's weight vector as: 
 		# w = np.random.randn(n) / sqrt(n), where n is the number of its inputs.
 
+		# Hyper-parameters
+		self.LAMBDA = lmbd
+		self.ETA = eta
+
+		# Weights and bias
 		self.W1 = np.random.normal(0,0.01,(self.NODES_IN_HIDDEN, data_dim))
 		self.W2 = np.random.normal(0,0.01,(num_labels, self.NODES_IN_HIDDEN))
 
 		self.b1 = np.zeros((self.NODES_IN_HIDDEN,1))
 		self.b2 = np.zeros((num_labels,1))
+
+		# Momentum
+		self.W1_moment = np.zeros(self.W1.shape)
+		self.W2_moment = np.zeros(self.W2.shape)
+		self.b1_moment = np.zeros(self.b1.shape)
+		self.b2_moment = np.zeros(self.b2.shape)
+
+		print 'Start Eta: %f' % self.ETA
 
 	def score_function(self, X, W, b):
 		return W*X+b
@@ -108,15 +123,34 @@ class NeuralNetwork:
 	def softmax_probabilities(self,scores):
 		return (np.exp(scores) / np.sum(np.exp(scores), axis=0))
 
-	def compute_cost(self,X,Y,W1,W2,b1,b2):
+	def compute_acc(self,y,X):
+		P = self.get_model_probs(X)
 
+		preds = np.argmax(P,axis=0).T
+		diff = preds - y
+		num_correct = diff[diff == 0].shape[1]
+		num_tot = y.shape[0]
+		acc = num_correct / float(num_tot)
+		return acc*100
+
+	def get_model_probs(self,X):
+		scores_hidden = self.score_function(X,self.W1,self.b1)
+		hidden_layer = self.relu_activation(scores_hidden)
+
+		scores = self.score_function(hidden_layer,self.W2,self.b2)
+		P = self.softmax_probabilities(scores)
+		return P		
+	
+	def compute_cost(self,X,Y):
+		"""
 		scores_hidden = self.score_function(X,W1,b1)
 		hidden_layer = self.relu_activation(scores_hidden)
 
 		# TODO: convert to vectorized implementation
 		scores = self.score_function(hidden_layer,W2,b2)
 		P = self.softmax_probabilities(scores)
-
+		"""
+		P = self.get_model_probs(X)
 		cross_entropy = 0
 		for i in range(0,X.shape[1]):
 			y = Y[:,i]
@@ -124,7 +158,7 @@ class NeuralNetwork:
 			ent = -np.log(y.T*p)
 			cross_entropy += ent
 		num_imgs = X.shape[1]
-		cost = (1/float(num_imgs))* cross_entropy + self.LAMBDA*np.sum(W1*W1)+self.LAMBDA*np.sum(W2*W2)
+		cost = (1/float(num_imgs))* cross_entropy + self.LAMBDA*np.sum(self.W1*self.W1)+self.LAMBDA*np.sum(self.W2*self.W2)
 		return cost
 
 
@@ -172,7 +206,7 @@ class NeuralNetwork:
 
 		return gradW1,gradb1,gradW2,gradb2
 
-	def train(self,Xbatches,Ybatches,X_valid,Y_valid, X_train, Y_train):
+	def train(self,Xbatches,Ybatches):
 		costs_train = []
 		costs_validation = []
 	
@@ -195,15 +229,26 @@ class NeuralNetwork:
 				## Gradient check 
 				#grad_check_sparse(lambda W: self.compute_cost(Xbatches[batch],Ybatches[batch],self.W1,self.W2,self.b1,self.b2), self.W2,gradW2,1)
 
-				# Update weights and bias
-				self.W1 += - self.ETA * gradW1
-				self.W2 += - self.ETA * gradW2
+				# Momentum update
+				self.W1_moment = self.MU * self.W1_moment - self.ETA * gradW1
+				self.W2_moment = self.MU * self.W2_moment - self.ETA * gradW2
+				self.b1_moment = self.MU * self.b1_moment - self.ETA * gradb1
+				self.b2_moment = self.MU * self.b2_moment - self.ETA * gradb2
 
-				self.b1 += - self.ETA * gradb1
-				self.b2 += - self.ETA * gradb2
-			costs_train = self.compute_cost(X_train, Y_train, self.W1, self.W2, self.b1, self.b2)
+				# Update weights and bias
+				self.W1 += self.W1_moment
+				self.W2 += self.W2_moment
+
+				self.b1 += self.b1_moment
+				self.b2 += self.b2_moment
+			
+			# Eta update
+			self.ETA = self.ETA * self.DECAY_RATE
+			#cost_train = self.compute_cost(X_train, Y_train)
 			#cost_validation = self.compute_cost(X_valid, Y_valid, self.W1, self.W2, self.b1, self.b2)
-			print("Cost train: %f" %costs_train)
+			#print 'Cost train: %f' % cost_train
+			#acc_train = self.compute_acc(y_train,X_train)
+			#print 'Accuracy train: %f' % acc_train
 			#print("Cost validation: %f" %cost_validation)
 
 def grad_check_sparse(f, x, analytic_grad, num_checks):
@@ -235,40 +280,24 @@ def rel_error(x, y):
   """ returns relative error """
   return np.max(np.abs(x - y) / (np.maximum(1e-8, np.abs(x) + np.abs(y))))
 
-def eval_numerical_gradient(f, x, verbose=True, h=0.00001):
-  """ 
-  a naive implementation of numerical gradient of f at x 
-  - f should be a function that takes a single argument
-  - x is the point (numpy array) to evaluate the gradient at
-  """ 
-
-  fx = f(x) # evaluate function value at original point
-  grad = np.zeros_like(x)
-  # iterate over all indexes in x
-  it = np.nditer(x, flags=['multi_index'], op_flags=['readwrite'])
-  while not it.finished:
-
-    # evaluate function at x+h
-    ix = it.multi_index
-    oldval = x[ix]
-    x[ix] = oldval + h # increment by h
-    fxph = f(x) # evalute f(x + h)
-    x[ix] = oldval - h
-    fxmh = f(x) # evaluate f(x - h)
-    x[ix] = oldval # restore
-
-    # compute the partial derivative with centered formula
-    grad[ix] = (fxph - fxmh) / (2 * h) # the slope
-    if verbose:
-      print ix, grad[ix]
-    it.iternext() # step to next dimension
-
-  return grad
-
 def main():
 	data = DataLoader()
 	network = NeuralNetwork(data.X_train.shape[0],data.Y_train.shape[0])
-	network.train(data.Xbatches, data.Ybatches, data.X_valid, data.Y_valid, data.X_train, data.Y_train)
+	network.train(data.Xbatches, data.Ybatches)
+
+	print(network.compute_acc(data.y_valid, data.X_valid))
 
 if __name__ == "__main__":
     main()
+
+
+
+
+"""
+Notes :
+
+Eta range : 0.3 - 0.002
+Lambda range : 0.1 - 1 
+
+
+"""
