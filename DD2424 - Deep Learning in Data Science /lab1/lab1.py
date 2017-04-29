lab1 gradient check fix

DD2424 - Deep Learning in Data Science /lab1/lab1.py
@@ -4,11 +4,12 @@ from scipy.optimize import check_grad
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
 from mpl_toolkits.axes_grid1 import ImageGrid
+from random import randrange
 
 NUM_BATCH_FILES = 5
 
 NUM_LABELS = 10
-LAMBD = 1
+LAMBD = 0
 ETA = 0.01
 N_BATCH = 100
 N_EPOCHS = 40
@@ -185,7 +186,8 @@ predictions for the images in X relative to the ground truth labels and
 the regularization term on W.
 """
 
-def compute_cost(X,Y,P,W):
+def compute_cost(X, Y, W, b):
+	P = evaluate_classifier(X, W, b)
 	cross_entropy = 0
 	for i in range(0,X.shape[1]):
 		y = Y[:,i]
@@ -274,38 +276,6 @@ def get_mini_batches_svm(Xtrain,y_train):
 		y_batches.append(y_batch)
 	return Xbatches,y_batches
 
-"""
-Numerical computation of the gradients. Used for checking of analytical gradients.
-"""
-def compute_grads_num(X, Y, W, b, lbda, h):
-    # Numerical check for gradients
-    no = W.shape[0]
-    d = X.shape[1]
-
-    grad_W = np.zeros(W.shape)
-    grad_b = np.zeros(no)
-
-    P = evaluate_classifier(X,W,b)
-
-    c = compute_cost(X, Y, P,W)
-
-    for i in range(no):
-        b_try = np.copy(b)
-        b_try[i] += h
-        P = evaluate_classifier(X,W,b)
-        c2 = compute_cost(X, Y, P, W)
-        grad_b[i] = (c2 - c) / h
-
-    for i in range(W.shape[0]):
-        for j in range(W.shape[1]):
-            W_try = np.copy(W)
-            W_try[i, j] = W_try[i, j] + h;
-            P = evaluate_classifier(X,W,b)
-            c2 = compute_cost(X, Y, P,W_try)
-            
-            grad_W[i, j] = (c2 - c) / h
-
-    return grad_W, grad_b
 
 """
 Train softmax regression classifier
@@ -324,15 +294,23 @@ def train_softmax_reg():
 
 	for epoch in range(0,N_EPOCHS):
 		for batch in range(0,len(Xbatches)):
+			
+			# Compute softmax probabilites
 			P = evaluate_classifier(Xbatches[batch],W,b)
+			
+			# Compute gradients
 			gradW, gradB = compute_gradients(Xbatches[batch],Ybatches[batch],P,W,b)
+			
+			# Gradient check
+			grad_check_sparse(lambda _: compute_cost(Xbatches[batch], Ybatches[batch], W, b), W, gradW, 1)
+
 			W = W - Eta*gradW
 			b = b - Eta*gradB
 		Eta = Eta * 0.9
 		P_validation = evaluate_classifier(Xtest,W,b)
 		P_train = evaluate_classifier(X,W,b)
-		cost_train = compute_cost(X,Y,P_train,W)
-		cost_validation = compute_cost(Xtest,Ytest,P_validation,W)
+		cost_train = compute_cost(X,Y,W,b)
+		cost_validation = compute_cost(Xtest,Ytest,W,b)
 		acc_train = compute_acc(y,P_train)
 		acc_validation = compute_acc(ytest,P_validation)
 		print("Cost train: %f" %cost_train)
@@ -377,6 +355,30 @@ def train_svm():
 		costs_train.append(cost_train)
 		costs_validation.append(cost_validation)
 
+def grad_check_sparse(f, x, analytic_grad, num_checks):
+  """
+  Adapted from: http://cs231n.github.io/neural-networks-case-study/ and http://cs231n.github.io/neural-networks-3/#gradcheck
+  sample a few random elements and only return numerical
+  in this dimensions.
+  """
+  h = 1e-5
+
+  x.shape
+  for i in xrange(num_checks):
+    ix = tuple([randrange(m) for m in x.shape])
+
+    oldval = x[ix]
+    x[ix] = oldval + h # increment by h
+    fxph = f(x) # evaluate f(x + h)
+    #print type(fxph), type(fxmh)
+    x[ix] = oldval - h # increment by h
+    fxmh = f(x) # evaluate f(x - h)
+    x[ix] = oldval # reset
+
+    grad_numerical = (fxph - fxmh) / (2 * h)
+    grad_analytic = analytic_grad[ix]
+    rel_error = abs(grad_numerical - grad_analytic) / (abs(grad_numerical) + abs(grad_analytic))
+    print 'numerical: %f analytic: %f, relative error: %e' % (grad_numerical, grad_analytic, rel_error)
 
 def plot_cost(costs_train,costs_validation):
 	epochs_arr = np.arange(0, N_EPOCHS).tolist()